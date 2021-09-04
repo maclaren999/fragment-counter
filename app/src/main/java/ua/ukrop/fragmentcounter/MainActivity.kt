834576ae1dcdfe7fc2fee289d6428922c2588b2b
maclaren99 <maclaren9999@gmail.com>
2021-09-04 17:45:59 +0300

    Initial commit. View created

---
+++ app/src/main/java/ua/ukrop/fragmentcounter/MainActivity.kt
@@ -0,0 +1,18 @@
+package ua.ukrop.fragmentcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.ukrop.fragmentcounter.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ua/ukrop/fragmentcounter/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ua.ukrop.fragmentcounter.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
